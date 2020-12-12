1828ecb6dd62dc2f0e15fe51000d3dd8334da756
thamma <thammaraku@gmail.com>
2020-12-12 01:01:23 -0600

    add questions

--- index.js
+++ index.js
@@ -12,9 +12,71 @@ var generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
-        name: 'projectTitle',
+        name: 'title',
         message: "What's your project title?",
-    }
+        default: 'none',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "What's your project description?",
+        default: 'none',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Could you provide steps to install your project?",
+        default: 'none',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: "Could you give an example of your project usage?",
+        default: 'none',
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: "Do you have any other contributors on this project?",
+        default: 'none',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: "If applicable, could you provide tests details of this project?",
+        default: 'none',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: "Please choose license from the list",
+        default: 'none',
+        choices: ['Mozilla Public License 2.0', 'MIT License', 'The Unlicense'],
+    },
 
 
 ];
@@ -24,7 +86,7 @@ function writeToFile(fileName, data) {
 
     fs.writeFile(fileName, data, (err) => {
         if (err) throw err;
-        console.log('The file has been saved!');
+        console.log('README file has been generated!');
     });
 
 }
