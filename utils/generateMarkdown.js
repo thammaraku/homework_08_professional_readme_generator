067aae5fd78479ec25cc4aac81eafc9fd2e82dae
thammaraku <thammaraku@gmail.com>
2020-12-13 01:26:58 -0600

    modify template

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,60 +1,81 @@
 // function to generate markdown for README
 function generateMarkdown(answers) {
+  let markDown = `# ${answers.title}
+        
+  ## Description
+  ${answers.description}
 
-
-  let markDown =
-
-    `# ${answers.title}
-
-    ![GitHub license](https://img.shields.io/badge/license-APACHE 2.0-blue.svg)
-    
-    ## Description
-    ${answers.description}
-
-    ## Table of Contents     
-      `
-  if (answers.installation !== "none") {
-    markDown += `
-      * [Installation](#installation)
-      To install necessary dependencies, run the following command:`
-
-      \`\`\`
-      ${answers.installation}
-      \`\`\`
-    `
-  };
-
-  if (answers.usage !== "none") {
-    markDown += `
-      * [Usage](#usage)
-      ${answers.usage}
-    `
-  };
-
-  if (answers.usage !== "none") {
-    markDown += `
-      * [Contributing](#contributing)
-      ${answers.contribute}
-      `
-  };
-
-  if (answers.test !== "none") {
-    markDown += `
-      * [Tests](#tests)
-      Instruction to test application:
-      ${answers.usage}
-        `
-  };
-
+  ## Table of Contents
   `
-    * [Questions](#questions)
-  Any questions, please contact ${ answers.email }.You can find more of my work at ${ answers.github }.
-    
-  ## License
-  This project is licensed under ${ answers.license }
+
+  if (answers.installation !== '') {
+    markDown +=
+  `* [Installation](#installation)
   `
+  };
+
+  if (answers.usage !== '') {
+    markDown +=
+  `* [Usage](#usage)
+  `
+  };
+
+  if (answers.usage !== '') {
+    markDown +=
+  `* [Contributing](#contributing)
+  `
+  };
+
+  if (answers.test !== '') {
+    markDown +=
+  `* [Tests](#tests)
+  `
+  }
+
+  // TEST SECTION
+
+  if (answers.installation !== '') {
+    markDown +=
+  `## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${answers.installation}
+  \`\`\`
+  `
+  };
+
+  if (answers.usage !== '') {
+    markDown +=
+  `## Usage
+  ${answers.usage}
+  `
+  };
+
+  `## License
+  This project is licensed under ${answers.license}
+  `
+
+  if (answers.contribute !== '') {
+    markDown +=
+  `## Contributing
+  ${answers.contribute}
+  `
+  };
+
+  if (answers.test !== '') {
+    markDown +=
+  `## Tests
+  Instruction to test application:
+  ${answers.usage}
+  `
+  };
+
+  markDown +=
+  `## Questions
+  Any questions, please contact ${answers.email}.You can find more of my work at ${answers.github}.
+  `
+
   return markDown;
-
 }
 
 module.exports = generateMarkdown;
