381ce2721bb77632498d4036c7da1c53d44b8efb
thammaraku <thammaraku@gmail.com>
2020-12-13 01:45:56 -0600

    add questions

--- index.js
+++ index.js
@@ -12,9 +12,30 @@ var generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
+        name: 'github',
+        message: "What's your github username?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What's your email address?",
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Your input is required otherwise default value will be used")
+            }
+            return true;
+        }
+    },
+    {
+        type: 'input',
         name: 'title',
         message: "What's your project title?",
-        default: 'none',
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("Your input is required otherwise default value will be used")
@@ -26,7 +47,6 @@ const questions = [
         type: 'input',
         name: 'description',
         message: "What's your project description?",
-        default: 'none',
         validate: function (answer) {
             if (answer.length < 1) {
                 return console.log("Your input is required otherwise default value will be used")
@@ -37,38 +57,27 @@ const questions = [
     {
         type: 'input',
         name: 'installation',
-        message: "Could you provide steps to install your project?",
-        default: 'none',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Your input is required otherwise default value will be used")
-            }
-            return true;
-        }
+        message: "Could you provide instruction to install your project? If any commands are needed, please provide on the next question",
+    },
+    {
+        type: 'input',
+        name: 'installation_command',
+        message: "Please list commands to install your proejct here",
     },
     {
         type: 'input',
         name: 'usage',
         message: "Could you give an example of your project usage?",
-        default: 'none',
     },
     {
         type: 'input',
         name: 'contribute',
         message: "Do you have any other contributors on this project?",
-        default: 'none',
     },
     {
         type: 'input',
         name: 'tests',
         message: "If applicable, could you provide tests details of this project?",
-        default: 'none',
-        validate: function (answer) {
-            if (answer.length < 1) {
-                return console.log("Your input is required otherwise default value will be used")
-            }
-            return true;
-        }
     },
     {
         type: 'list',
